1b783ac4310be211e3ba2cdb21e28e3f88e8988c
Divyansh Verma <divyanshmotog5g@gmail.com>
2025-09-03 00:36:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function renderDate() {
+	const date = new Date()
+	document.getElementById("clock").innerHTML = date.toLocaleString().toUpperCase()
+}
+
+setInterval(() => {
+	renderDate()
+})
