2d2a6516c645df9f3a5fb969f134c7e4e7068433
Divyansh Verma <divyanshmotog5g@gmail.com>
2025-09-03 00:44:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //your JS code here. If required.
 function renderDate() {
 	const date = new Date()
-	document.getElementById("clock").innerHTML = date.toLocaleString().toUpperCase()
+	document.getElementById("timer").innerHTML = date.toLocaleString().toUpperCase()
 }
 
 setInterval(() => {
